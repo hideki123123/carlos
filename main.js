6399487ebd7e466b06cf6474e79d1d5019f57424
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-03-08 11:44:01 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-prompt('qual é seu nome?');
+var nomeUsuario = prompt('qual é seu nome?');
+var texto = document.querySelector('span');
+
+texto.textContent = nomeUsuario;
+
+while(nomeUsuario ==''){
+    nomeUsuario = prompt('qual é seu nome?')
+}
