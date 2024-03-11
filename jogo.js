22ab17f38abe86dd48e1b5f73d7ad85c51a4e9bc
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-03-11 12:16:23 -0300

    vb

--- jogo.js
+++ jogo.js
@@ -1,9 +1,12 @@
 let numeroSecreto = Math.random() * 10;
 console.log(numeroSecreto);
 let chuteV = 0;
+let chute = '';
 
-while(!chuteV){
+while(chute == ''){
     chute = prompt('Escolha um número de 0 a 10');
+}
+while(!chuteV || chute != ''){
     if(chute == numeroSecreto){
         alert('Parabéns, você acertou');
         chute = 1;
