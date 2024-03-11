eff221d17012f8e5dd83fb3523f12b26bb51f89f
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-03-11 12:13:01 -0300

    dfg

--- jogo.js
+++ jogo.js
@@ -1,12 +1,16 @@
 let numeroSecreto = Math.random() * 10;
-
+console.log(numeroSecreto);
+let chuteV = 0;
 let chute = '';
 
 while(chute == ''){
     chute = prompt('Escolha um número de 0 a 10');
 }
+while(!chuteV){
     if(chute == numeroSecreto){
         alert('Parabéns, você acertou');
+        chute = 1;
     }else{
         alert('Que triste, FDS');
     }   
+}
