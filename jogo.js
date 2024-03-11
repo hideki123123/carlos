c97ad54fd883ce80acb6f5197b00b877fef8341d
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-03-11 11:53:53 -0300

    asd

--- jogo.js
+++ jogo.js
@@ -1,11 +1,12 @@
 let numeroSecreto = 9;
 
-let chute = prompt('Escolha um número de 0 a 10');
-while(chute == ''){
+let chute = '';
 
+while(chute == ''){
+    let chute = prompt('Escolha um número de 0 a 10');
+}
     if(chute == numeroSecreto){
         alert('Parabéns, você acertou');
     }else{
         alert('Que triste, FDS');
     }   
-}
