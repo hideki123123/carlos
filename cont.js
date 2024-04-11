b900f48a649fb3e01bac3623f22d11b68b0ed9fb
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-04-11 09:23:44 -0300

    asd

--- cont.js
+++ cont.js
@@ -1,12 +1,21 @@
 const B = document.querySelectorAll('.objetivos');
-console.log(B.length);
+const textos = document.querySelectorAll('.aba-cont');
+
+
+
+
+
 
 for(let i = 0; i < B.length; i++){
     console.log(i);
     B[i].onclick = function(){
+
      for(let j = 0; j < B.length; j++){
         B[j].classList.remove('select');
+        textos[j].classList.remove('select2');
      }
+
     B[i].classList.add('select');
+    textos[i].classList.add('select2');
     }
 }
