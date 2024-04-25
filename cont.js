b0a6e09e972f3f5e208a73850b508a3b33873438
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-04-25 09:11:58 -0300

    asdc

--- cont.js
+++ cont.js
@@ -4,15 +4,22 @@ const textos = document.querySelectorAll('.aba-cont');
 
 
 for(let i = 0; i < B.length; i++){
-    console.log(i);
     B[i].onclick = function(){
-
-     for(let j = 0; j < B.length; j++){
+    for(let j = 0; j < B.length; j++){
         B[j].classList.remove('select');
         textos[j].classList.remove('select2');
-     }
-
+    }
     B[i].classList.add('select');
     textos[i].classList.add('select2');
     }
 }
+const contadores = document.querySelectorAll('contador');
+let tempoAtual = new Date();
+let tempoObjetivo1 = nem Date ('2024-04-19T00:00:00');
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
+
+contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
