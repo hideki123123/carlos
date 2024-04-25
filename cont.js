6d02e9ac9e2b2fbdeb8eb4eef4aaa35313ba8cfb
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-04-25 09:16:52 -0300

    zzxc

--- cont.js
+++ cont.js
@@ -16,10 +16,23 @@ for(let i = 0; i < B.length; i++){
 const contadores = document.querySelectorAll('contador');
 let tempoAtual = new Date();
 let tempoObjetivo1 = new Date ('2024-04-19T00:00:00');
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+let tempoObjetivo2 = new Date ('2024-05-19T00:00:00');
+let tempoObjetivo3 = new Date ('2024-06-19T00:00:00');
+let tempoObjetivo4 = new Date ('2024-07-19T00:00:00');
+
+let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
+
+
+function CalculaTempo(tempoObjetivo){
+let tempoFinal = tempoObjetivo - tempoAtual;
 let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
 
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
 contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
