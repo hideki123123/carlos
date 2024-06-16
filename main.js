ba991da82209fa71e5e7b3fa158c930ed1f4c0be
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-06-16 17:06:57 -0300

    fsdf

--- main.js
+++
@@ -1,8 +0,0 @@
-var nomeUsuario = prompt('qual é seu nome?');
-var texto = document.querySelector('span');
-
-texto.textContent = nomeUsuario;
-
-while(nomeUsuario ==''){
-    nomeUsuario = prompt('qual é seu nome?')
-}
